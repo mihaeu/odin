547aceae5bf193425e419d9aa0650b19928c72d6
Michael Haeuslmann <haeuslmann@gmail.com>
2013-12-11 16:12:36 +0545

    Solved the problem with loading the config using Reflection to determine Composer path.

--- src/Mihaeu/Odin/Configuration/YamlConfiguration.php
+++ src/Mihaeu/Odin/Configuration/YamlConfiguration.php
@@ -24,8 +24,14 @@ class YamlConfiguration implements ConfigurationInterface
      */
     public function __construct()
     {
-        $configFile = file_exists('config.yml') ? 'config.yml' : '../config.yml';
-        $this->config = YAML::parse($configFile);
+//        $configFile = file_exists('config.yml') ? 'config.yml' : '../config.yml';
+
+        // since this project depends on composer and the composer ClassLoader is
+        // always in the same directory, we can use Reflection to find its
+        // location and the base directory located two levels below
+        $reflector = new \ReflectionClass('Composer\Autoload\ClassLoader');
+        $configDirectory = realpath(dirname($reflector->getFileName()).'/../..');
+        $this->config = YAML::parse($configDirectory.'/config.yml');
     }
 
     /**
