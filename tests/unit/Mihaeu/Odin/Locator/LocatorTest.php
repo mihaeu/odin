a1410d0ced983c81e183174fea0d59f40159b916
Michael Haeuslmann <haeuslmann@gmail.com>
2014-02-21 11:21:55 +0330

    Added basic config for PHPUnit and Travis.

---
+++ tests/unit/Mihaeu/Odin/Locator/LocatorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Class LocatorTest.php
+ *
+ * @author Michael Haeuslmann <haeuslmann@gmail.com>
+ */
+class LocatorTest extends PHPUnit_Framework_TestCase
+{
+    public function testTestPhpUnitSetup()
+    {
+        $this->assertTrue(false);
+    }
+}
